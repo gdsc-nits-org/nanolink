89971f493b63708ca3e10e6e70af3760d44a4926
ruler45 <sahin0945@gmail.com>
2024-04-09 15:39:03 +0530

    UPD:some build improvements

--- src/controllers/UrlShortener/UrlShortener.ts
+++ src/controllers/UrlShortener/UrlShortener.ts
@@ -11,6 +11,28 @@ const UrlShortener: Interfaces.Controllers.Auth = async (req, res, next) => {
     moment.locale("en");
     const time = moment().format("MMMM Do YYYY, h:mm:ss a");
     const { originalUrl } = req.body;
+    if (!originalUrl) {
+      return next(Utils.Response.error("Please provide a url", 404));
+    } else if (
+      !originalUrl.startsWith("http") ||
+      !originalUrl.startsWith("https")
+    ) {
+      return next(
+        Utils.Response.error(
+          "Your URL should start with 'http' or 'https' ",
+          403
+        )
+      );
+    }
+    const existingUrl = await prisma.url.findFirst({
+      where: {
+        originalUrl,
+        userId: req.user.id,
+      },
+    });
+    if (existingUrl) {
+      return next(Utils.Response.error("Url already shortened", 403));
+    }
     const shortId = nanoid(6);
     const shortUrl = `${req.hostname}/${shortId}`;
     const result = await prisma.url.create({
